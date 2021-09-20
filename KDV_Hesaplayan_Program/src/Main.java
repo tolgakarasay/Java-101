f2a5a541ce8f5afa1b24430a8b0e5d99c7b5417c
Tolga Karasay <tolgakarasay@gmail.com>
2021-09-20 03:08:10 +0300

    KDV commit 2

--- KDV_Hesaplayan_Program/src/Main.java
+++ KDV_Hesaplayan_Program/src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double tutar, kdv, kdvliTutar, kdvOrani;
+        System.out.println("Tutarı giriniz:");
+        Scanner scanner = new Scanner(System.in);
+        tutar = scanner.nextDouble();
+        scanner.close();
+
+        kdvOrani = tutar <= 1000 ? 0.18 : 0.08;
+        kdv = kdvOrani * tutar;
+        kdvliTutar = tutar + kdv;
+
+        System.out.println("KDV oranı: " + kdvOrani);
+        System.out.println("KDV tutarı: "+ kdv);
+        System.out.println("KDV'li tutar: " + kdvliTutar);
+    }
 }
