009807b15bdd29abbaa1f1a1229964b42901bd02
Tolga Karasay <tolgakarasay@gmail.com>
2021-09-23 21:29:38 +0300

    ebob ekok bulmak

--- EBOB_EKOK_Bulan_Program/src/Main.java
+++ EBOB_EKOK_Bulan_Program/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int n1, n2, min, ebob=1, ekok;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = scanner.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = scanner.nextInt();
+        scanner.close();
+
+        if(n1<=n2){
+            min = n1;
+        } else {
+            min = n2;
+        }
+
+        int i = 1;
+        while(i<=min){
+            if(n1%i==0 && n2%i==0){
+                ebob = i;
+            }
+            i++;
+        }
+
+        ekok = n1*n2/ebob;
+
+        System.out.println("EBOB : "+ebob);
+        System.out.println("EKOK : "+ekok);
     }
 }
